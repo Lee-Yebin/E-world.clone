1c50433d174544dc590bf4243802e103c7507e6b
yebin lee <84556082+Lee-Yebin@users.noreply.github.com>
2021-07-21 10:36:10 +0900

    e-world

---
+++ js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+$(document).ready(function(){
+    $('.main-banner__list').slick({
+        slidesToShow: 1, 
+        slidesToScroll: 1, 
+        autoplay: true, 
+        autoplaySpeed: 5000, 
+        arrows: false, 
+        dots: true
+    });
+    });
